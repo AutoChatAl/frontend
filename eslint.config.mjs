b47c9095551776db7e185ad4880c52d1055e0fcb
Diego Dalla Bernardina Thedoldi <diegodallabt01@gmail.com>
2026-03-23 00:15:52 -0300

    deploy: fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+import { FlatCompat } from '@eslint/eslintrc';
 import pluginImport from 'eslint-plugin-import';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
 const config = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: [
       '.next/**',
